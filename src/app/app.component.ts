3b884b218daadbd58d0114e62d6ddcc3647bead7
Vitalij Schwab <schwab.vitalij@gmail.com>
2024-05-28 22:34:40 +0200

    create contact component design left container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillSetComponent } from './skill-set/skill-set.component';
 import { MyWorkComponent } from './my-work/my-work.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AboveTheFoldComponent, AboutMeComponent, SkillSetComponent, MyWorkComponent],
+  imports: [RouterOutlet, AboveTheFoldComponent, AboutMeComponent, SkillSetComponent, MyWorkComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
